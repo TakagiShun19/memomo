bb47b714b47d803be16db38dd83ac9b17929b198
高木駿 <stakagi@takagishunnombp.lan>
2019-07-10 14:35:32 +0900

    開発向け修正

--- app/User.php
+++ app/User.php
@@ -10,9 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
 
-    use Notifiable;
-
-    use ModelConfig;
+    use Notifiable, ModelConfig, HasApiTokens;
 
     public $incrementing = false;
 
